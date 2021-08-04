357ad89a3b43f8ce1bdea6c75409dd218358ba54
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-08-04 19:24:43 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ return [
     "username" => "root",
     "password" => "root",
     "dbname" => "w1020",
+    "dbname" => "sales",
     "page_size" => 5,
     "salt"=>"kjsksdjt5859fdjkdfdfj"
 //    "table" => "ved"
